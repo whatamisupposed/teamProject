9aaf488fe4e4711a6fbb44966ef7f14805c3daea
Emily Twyman <twymanem@gmail.com>
2024-07-11 10:21:00 -0600

    hoi

---
+++ my-react-app/server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 const express = require('express');
