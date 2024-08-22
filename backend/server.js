00ef9f473da556193ec52795cd66497eb615330d
Cassius Hill <MarcelusWork@gmail.com>
2024-08-21 18:51:47 -0600

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Cors
+const allowedOrigins = [
+    'http://localhost:5173', 
+    'https://capstone-mtech-frontend.onrender.com' 
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: function(origin, callback) {
+        
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true 
 }));
 
